395a89dedcc880bef4f85c44e988896c836ebeec
B4 溝部　壮太朗 <122212879+a073ffu@users.noreply.github.com>
2023-12-19 15:15:11 +0900

    Update sequence_list_generator.py

--- src/sequence_list_generator.py
+++ src/sequence_list_generator.py
@@ -9,46 +9,46 @@ class Gen():
     # トークンに応じた count の変化量を dict で定義
     POINT_OF_COUNT={'A':1, 'B':1, 'C':1, 'D':-1, 'E':-1, 'F':-1, 'G':-1, 'H':-1, 'I':-2, 'J':-2, 'K':1, 'L':1, 'M':-1, 'N':-4}
     
-    # トークンに応じた comp （トークンの生成される確率の底２の対数のマイナスの総和）の変化量を dict で定義
-    POINT_OF_COMP={'A':math.log(5, 2), 'B':math.log(5, 2), 'C':math.log(5, 2), 'K':math.log(5, 2), 'L':math.log(5, 2), 'D':math.log(11, 2), 'E':math.log(11, 2), 'F':math.log(11, 2), 'G':math.log(11, 2), 'H':math.log(11, 2), 'I':math.log(11, 2), 'J':math.log(13, 2), 'M':math.log(13, 2), 'N':math.log(14, 2)}
-    
-    # generate_token_reduce_count ではトークンの生成される確率が変わる
-    MINUSPOINT_OF_COMP={'A':0, 'B':0, 'C':0, 'K':0, 'L':0, 'D':math.log(6, 2), 'E':math.log(6, 2), 'F':math.log(6, 2), 'G':math.log(6, 2), 'H':math.log(6, 2), 'I':math.log(6, 2), 'J':math.log(8, 2), 'M':math.log(8, 2), 'N':math.log(9, 2)}
-
     def __init__(self):
         self.count=0 #リストをProgramStack()でオブジェクト化した時の要素数
         self.seq=[]
-        self.comp=0
+        self.comp=0 #複雑度
     
     def generate_token(self, r): #rの値に応じてseqにトークンを追加
         self.seq.append(self.TOKUNS[r])
-        self.update_count_and_comp(r)
+        self.update_count(r)
+    
+    def update_count(self, r): #rの値に応じてcountを増減
+        self.count+=self.POINT_OF_COUNT[self.TOKUNS[r]]   
     
-    def update_count_and_comp(self, r): #rの値に応じてcountを増減
-        self.count+=self.POINT_OF_COUNT[self.TOKUNS[r]]
-        self.comp+=self.POINT_OF_COMP[self.TOKUNS[r]]
-    
-    def reduce_count_and_update_comp(self, r):
-        self.count+=self.POINT_OF_COUNT[self.TOKUNS[r]]
-        self.comp+=self.MINUSPOINT_OF_COMP[self.TOKUNS[r]]
-    
-    def generate_token_change_count(self):
-        if (self.count > 4): #オブジェクトが5個以上
-            self.generate_token(r.randint(0, 13)) # すべてのトークン
-        elif (self.count > 2): #オブジェクトが3個以上
-            self.generate_token(r.randint(0, 12)) # loop2を除く
-        elif (self.count > 1): #オブジェクトが2個以上
-            self.generate_token(r.randint(0, 10)) # cond, loop, loop2を除く
-        else: #オブジェクトなし
-            self.generate_token(r.randint(0, 4)) # 定数、変数のみ
+    def generate_token_and_update_comp(self): #引数として渡されうる数の個数の底２の対数を comp に加える
+        if (self.count > 4): #オブジェクトが5個以上
+            # すべてのトークン
+            self.generate_token(r.randint(0, 13))
+            self.comp+=math.log(14, 2) #渡されうる数は0~13なので、14個あるため、14の底2の対数を comp に加える
+        elif (self.count > 2): #オブジェクトが3個以上
+            # loop2を除く
+            self.generate_token(r.randint(0, 12))
+            self.comp+=math.log(13, 2)
+        elif (self.count > 1): #オブジェクトが2個以上
+            # cond, loop, loop2を除く
+            self.generate_token(r.randint(0, 10))
+            self.comp+=math.log(11, 2)
+        else: #オブジェクト1個以下
+            # 定数、変数のみ
+            self.generate_token(r.randint(0, 4))
+            self.comp+=math.log(5, 2)
 
-    def generate_token_reduce_count(self): #change_count()から定数、変数の追加をなくした
-        if (self.count > 4):
-            self.generate_token(r.randint(5, 13)) 
-        elif (self.count > 2):
-            self.generate_token(r.randint(5, 12)) 
-        elif (self.count > 1):
-            self.generate_token(r.randint(5, 10)) 
+    def generate_token_and_update_comp_reduce_count(self): #　generate_token_and_update_comp() から定数、変数の追加をなくした
+        if (self.count > 4):
+            self.generate_token(r.randint(5, 13))
+            self.comp+=math.log(9, 2)
+        elif (self.count > 2):
+            self.generate_token(r.randint(5, 12))
+            self.comp+=math.log(8, 2)
+        elif (self.count > 1):
+            self.generate_token(r.randint(5, 10))
+            self.comp+=math.log(6, 2)
 
     def count_info(self): #countを出力
         return self.count
@@ -99,11 +99,11 @@ def generate():
     n=0
     a=1
     while(a>n):
-        seq.change_count()
+        seq.generate_token_and_update_comp()
         n+=1
         a=r.randint(0, 100)
         
     while(seq.count_info()>1):
-        seq.reduce_count()
+        seq.generate_token_and_update_comp_reduce_count()
     
     return seq.seq_info(), seq.comp_info()
