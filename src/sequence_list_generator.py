382862154c4f9b13e35c029b2ede0fc1259d357d
B4 溝部　壮太朗 <122212879+a073ffu@users.noreply.github.com>
2023-12-26 11:52:08 +0900

    Update sequence_list_generator.py

--- src/sequence_list_generator.py
+++ src/sequence_list_generator.py
@@ -1,109 +1,72 @@
 #A=0 B=1 C=2 D=+ E=- F=* G=div H=mod I=cond J=loop K=x L=y M=compr N=loop2
-import random as r
+import random
 import math
+import sequence_generator
 
-class Gen(): 
-    # countの数に対応する呼び出せる文字を randint() でまとめて指定できるように並び替え
-    TOKUNS=['A', 'B', 'C', 'K', 'L', 'D', 'E', 'F', 'G', 'H', 'M', 'I', 'J', 'N']
-    
+class ProgramGenerator(): 
     # トークンに応じた count の変化量を dict で定義
     POINT_OF_COUNT={'A':1, 'B':1, 'C':1, 'D':-1, 'E':-1, 'F':-1, 'G':-1, 'H':-1, 'I':-2, 'J':-2, 'K':1, 'L':1, 'M':-1, 'N':-4}
     
     def __init__(self):
-        self.count=0 #リストをProgramStack()でオブジェクト化した時の要素数
-        self.seq=[]
-        self.comp=0 #複雑度
+        self.program_num=0 #リストをProgramStack()でオブジェクト化した時の要素数
+        self.sequence=[]
+        self.information_amount=0 #複雑度
+        self.letter_of_choice={}
+        self.max_variation_of_program_num=1
     
-    def generate_token(self, r): #rの値に応じてseqにトークンを追加
-        self.seq.append(self.TOKUNS[r])
-        self.update_count(r)
+    def generate_token(self): #rの値に応じてseqにトークンを追加
+        self.append_letter()
+
+    def append_letter(self):
+        min_variation, max_variation = 1-self.program_num, self.max_variation_of_program_num
+        letter, information_amount= self.generate_random_letter(min_variation, max_variation)
+        self.sequence.append(letter)
+        self.update_program_num(letter)
+        self.add_information_amount(information_amount)
+
+    def generate_random_letter(self, min_variation, max_variation):
+        if (self.letter_of_choice.get(min_variation, [])):
+            return random.choice(self.letter_of_choice[min_variation]), math.log(len(self.letter_of_choice[min_variation]), 2)    
+        else:
+            letters = []
+            letter2variation = {'A':1, 'B':1, 'C':1, 'D':-1, 'E':-1, 'F':-1, 'G':-1, 'H':-1, 'I':-2, 'J':-2, 'K':1, 'L':1, 'M':-1, 'N':-4}
+            for k, v in letter2variation.items():
+                if min_variation <= v <= max_variation:
+                    letters.append(k)
+            if(max_variation==1):
+                self.letter_of_choice[min_variation]=letters
+            return random.choice(letters), math.log(len(letters), 2)
+    
+    def change_max_variation_of_program_num(self):
+        self.max_variation_of_program_num=-1
+        self.letter_of_choice={}
+
+    def add_information_amount(self, information_amount):
+        self.information_amount+=information_amount
     
-    def update_count(self, r): #rの値に応じてcountを増減
-        self.count+=self.POINT_OF_COUNT[self.TOKUNS[r]]   
+    def update_program_num(self, r): #rの値に応じてcountを増減
+        self.program_num+=self.VARIATION_OF_PROGRAM_NUM[r] 
     
-    def generate_token_and_update_comp(self): #引数として渡されうる数の個数の底２の対数を comp に加える
-        if (self.count > 4): #オブジェクトが5個以上
-            # すべてのトークン
-            self.generate_token(r.randint(0, 13))
-            self.comp+=math.log(14, 2) #渡されうる数は0~13なので、14個あるため、14の底2の対数を comp に加える
-        elif (self.count > 2): #オブジェクトが3個以上
-            # loop2を除く
-            self.generate_token(r.randint(0, 12))
-            self.comp+=math.log(13, 2)
-        elif (self.count > 1): #オブジェクトが2個以上
-            # cond, loop, loop2を除く
-            self.generate_token(r.randint(0, 10))
-            self.comp+=math.log(11, 2)
-        else: #オブジェクト1個以下
-            # 定数、変数のみ
-            self.generate_token(r.randint(0, 4))
-            self.comp+=math.log(5, 2)
-
-    def generate_token_and_update_comp_reduce_count(self): #　generate_token_and_update_comp() から定数、変数の追加をなくした
-        if (self.count > 4):
-            self.generate_token(r.randint(5, 13))
-            self.comp+=math.log(9, 2)
-        elif (self.count > 2):
-            self.generate_token(r.randint(5, 12))
-            self.comp+=math.log(8, 2)
-        elif (self.count > 1):
-            self.generate_token(r.randint(5, 10))
-            self.comp+=math.log(6, 2)
-
-    def count_info(self): #countを出力
-        return self.count
+    def program_num_info(self): # program_num を出力
+        return self.program_num
     
-    def seq_info(self): #seqを出力
-        return self.seq
+    def sequence_info(self): # sequence を出力
+        return self.sequence
     
-    def comp_info(self):
-        return self.comp
-
-# テスト用翻訳関数
-def honyaku(seq):
-    for i in range(len(seq)):
-        if seq[i]=='A':
-            seq[i]='0'
-        elif seq[i]=='B':
-            seq[i]='1'
-        elif seq[i]=='C':
-            seq[i]='2'
-        elif seq[i]=='D':
-            seq[i]='+'
-        elif seq[i]=='E':
-            seq[i]='-'
-        elif seq[i]=='F':
-            seq[i]='*'
-        elif seq[i]=='G':
-            seq[i]='div'
-        elif seq[i]=='H':
-            seq[i]='mod'
-        elif seq[i]=='I':
-            seq[i]='cond'
-        elif seq[i]=='J':
-            seq[i]='loop'
-        elif seq[i]=='K':
-            seq[i]='x'
-        elif seq[i]=='L':
-            seq[i]='y'
-        elif seq[i]=='M':
-            seq[i]='compr'
-        elif seq[i]=='N':
-            seq[i]='loop2'
-    
-    return seq
-
+    def information_amount_info(self): # information_amount を出力
+        return self.information_amount
 
 def generate():
-    seq=Gen()
+    sequence=ProgramGenerator()
     n=0
     a=1
     while(a>n):
-        seq.generate_token_and_update_comp()
+        sequence.generate_token()
         n+=1
-        a=r.randint(0, 100)
-        
-    while(seq.count_info()>1):
-        seq.generate_token_and_update_comp_reduce_count()
+        a=random.randint(0, 100)
+
+    sequence.change_max_variation_of_program_num()
+    while(sequence.program_num_info()>1):
+        sequence.generate_token()
     
-    return seq.seq_info(), seq.comp_info()
+    return sequence.sequence_info(), sequence.information_amount_info()
